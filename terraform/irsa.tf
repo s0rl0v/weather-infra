13e9cd4ad1ef3f47338fd6a4b106c12a530d2696
Sergey Orlov <serhio.orlovsky@gmail.com>
2022-12-05 17:33:05 +0700

    add outputs for irsa roles

--- terraform/irsa.tf
+++ terraform/irsa.tf
@@ -39,3 +39,17 @@ module "aws-eks-image-updater-irsa" {
     }
   }
 }
+
+module "aws-eks-external-secrets-irsa" {
+  source    = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  role_name = "ExternalSecretsRole"
+
+  attach_external_secrets_policy = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["external-secrets:external-secrets"]
+    }
+  }
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,17 @@ output iam_role_aws_lb_controller {
   value       = module.aws-lb-controller-irsa.iam_role_name
   description = "IAM Role name for AWS Load Balancer controller"
 }
+
+output iam_role_eks_autoscaler {
+  value       = module.aws-eks-autoscaler-irsa.iam_role_name
+  description = "IAM Role name for AWS Autoscaler controller"
+}
+output iam_role_aws_eks_image_updater_irsa {
+  value       = module.aws-eks-image-updater-irsa.iam_role_name
+  description = "IAM Role name for ArgoCD image updater"
+}
+
+output iam_role_aws_eks_external_secrets_irsa {
+  value       = module.aws-eks-external-secrets-irsa.iam_role_name
+  description = "IAM Role name for External Secrets"
+}
